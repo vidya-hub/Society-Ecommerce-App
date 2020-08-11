ca5d21da5d82efe90f273943394dd8147c5be1d3
Payel <61454146+payelmasanta@users.noreply.github.com>
2020-08-11 21:32:19 +0530

    Update add_store.dart

--- lib/screens/add_store.dart
+++ lib/screens/add_store.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:society/screens/add_product_screen.dart';
 import 'package:society/screens/screen13.dart';
 import 'package:society/screens/screen16.dart';
@@ -22,7 +22,7 @@ class _AddStoreState extends State<AddStore> {
   File _image;
   final picker = ImagePicker();
   Item selectedUser;
-  
+
   List<Item> users = <Item>[
     const Item(
       'Boutique',
@@ -30,16 +30,15 @@ class _AddStoreState extends State<AddStore> {
     const Item(
       'Groceries',
     ),
-     const Item(
+    const Item(
       'Gadgets',
     ),
     const Item(
       'Games',
     ),
-     const Item(
+    const Item(
       'Books',
     ),
-   
   ];
 
   Future getImage() async {
@@ -52,13 +51,14 @@ class _AddStoreState extends State<AddStore> {
 
   @override
   Widget build(BuildContext context) {
+    var selectedCategory, selectedType;
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       body: Container(
           child: Padding(
         padding: const EdgeInsets.all(6.0),
         // padding: const EdgeInsets.only(top: 40.0, left: 6.0),
-        child:ListView(
+        child: ListView(
           children: <Widget>[
             Stack(
               children: [
@@ -120,124 +120,162 @@ class _AddStoreState extends State<AddStore> {
                 )
               ],
             ),
-            Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  TextFormField(
-                    cursorColor: Colors.black,
-                    validator: validatetName,
-                    onSaved: (value) {
-                      _name = value;
-                    },
-                    keyboardType: TextInputType.text,
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                        // icon: Icon(Icons.people, color: Colors.black),
-                        labelText: "Add Store Title",
-                        border: InputBorder.none,
-                        labelStyle: TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.w600)),
+            Column(mainAxisAlignment: MainAxisAlignment.start, children: <
+                Widget>[
+              TextFormField(
+                cursorColor: Colors.black,
+                validator: validatetName,
+                onSaved: (value) {
+                  _name = value;
+                },
+                keyboardType: TextInputType.text,
+                controller: _nameController,
+                decoration: InputDecoration(
+                    // icon: Icon(Icons.people, color: Colors.black),
+                    labelText: "Add Store Title",
+                    border: InputBorder.none,
+                    labelStyle:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.w600)),
+              ),
+              TextFormField(
+                cursorColor: Colors.black,
+                validator: validatetName,
+                onSaved: (value) {
+                  _name = value;
+                },
+                keyboardType: TextInputType.text,
+                controller: _nameController,
+                decoration: InputDecoration(
+                    // icon: Icon(Icons.people, color: Colors.black),
+                    labelText: "Add store decription",
+                    border: InputBorder.none,
+                    labelStyle: TextStyle(
+                      fontSize: 20,
+                    )),
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 10.0,
+                ),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
                   ),
-                  TextFormField(
-                    cursorColor: Colors.black,
-                    validator: validatetName,
-                    onSaved: (value) {
-                      _name = value;
-                    },
-                    keyboardType: TextInputType.text,
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                        // icon: Icon(Icons.people, color: Colors.black),
-                        labelText: "Add store decription",
-                        border: InputBorder.none,
-                        labelStyle: TextStyle(
-                          fontSize: 20,
-                        )),
-                  ),
-                  SizedBox(
-                    height: 70,
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(left: 10.0,),
-                      child:  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                    ),
-                    child: Container(
-                      width: 370,
-                      height: 60,
-                      decoration: new BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: new BorderRadius.circular(10),
-                          border: Border.all(color: Colors.lightGreen, width: 2)),
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Row(children: <Widget>[
-                            Expanded(
-                              child: Text(
-                                "Store Category *",
-                                style: TextStyle(fontSize: 17,fontFamily: "Times new Roman"),
+                  child: Container(
+                    width: 370,
+                    height: 60,
+                    decoration: new BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: new BorderRadius.circular(10),
+                        border: Border.all(color: Colors.lightGreen, width: 2)),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Store Category",
+                                style: TextStyle(
+                                    fontSize: 17,
+                                    fontFamily: "Times new Roman"),
                                 textAlign: TextAlign.left,
                               ),
-                            ),
-                            Expanded(
-                              child: Center(
-                                child: DropdownButton<Item>(
-                                  hint: Text(
-                                    'Select Catagory',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  value: selectedUser,
-                                  onChanged: (Item Value) {
-                                    setState(() {
-                                      selectedUser = Value;
-                                    });
-                                  },
-                                  items: users.map((Item user) {
-                                    return DropdownMenuItem<Item>(
-                                      value: user,
-                                      child: Row(children: <Widget>[
-                                        Text(
-                                          user.name,
-                                          style: TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 16,
-                                              fontFamily: "GentiumBasic"),
-                                        ),
-                                      ]),
-                                    );
-                                  }).toList(),
-                                ),
-                              ),
-                            ),
-                          ]),
-                        ),
+                              //
+                              SizedBox(height: 40.0),
+                              StreamBuilder<QuerySnapshot>(
+                                  stream: Firestore.instance
+                                      .collection("category2")
+                                      .snapshots(),
+                                  builder: (context, snapshot) {
+                                    if (!snapshot.hasData)
+                                      const Text("Loading.....");
+                                    else {
+                                      List<DropdownMenuItem> categoryItems = [];
+                                      for (int i = 0;
+                                          i < snapshot.data.documents.length;
+                                          i++) {
+                                        DocumentSnapshot snap =
+                                            snapshot.data.documents[i];
+                                        categoryItems.add(
+                                          DropdownMenuItem(
+                                            child: Text(
+                                              snap.documentID,
+                                              style: TextStyle(),
+                                            ),
+                                            value: "${snap.documentID}",
+                                          ),
+                                        );
+                                      }
+                                      return Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          // Icon(FontAwesomeIcons.coins,
+                                          //     size: 25.0, color: Color(0xff11b719)),
+                                          SizedBox(width: 50.0),
+                                          DropdownButton(
+                                            items: categoryItems,
+                                            onChanged: (categoryValue) {
+                                              final snackBar = SnackBar(
+                                                content: Text(
+                                                  'Selected store Category is $categoryValue',
+                                                  style: TextStyle(
+                                                      color: Color(0xff11b719)),
+                                                ),
+                                              );
+                                              Scaffold.of(context)
+                                                  .showSnackBar(snackBar);
+                                              setState(() {
+                                                selectedCategory =
+                                                    categoryValue;
+                                              });
+                                            },
+                                            value: selectedCategory,
+                                            isExpanded: false,
+                                            hint: new Text(
+                                              "Select category",
+                                              style: TextStyle(
+                                                  color: Colors.grey[800],
+                                                  fontWeight: FontWeight.w500),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    }
+                                  }),
+                              //
+                            ]),
                       ),
                     ),
                   ),
-                  ),
-                ]),
-            Padding(padding: EdgeInsets.only(top:60),
-           child: Container(
-              width: MediaQuery.of(context).size.width,
-              child: RaisedButton(
-                color: Color.fromRGBO(1, 44, 50, 0.8),
-                onPressed: () {
-                   Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => Screen13()),
-  );
-                },
-                child: Text(
-                  'CREATE STORE NOW',
-                  style: TextStyle(
-                      color: Colors.white,
-                      // fontWeight: FontWeight.bold,
-                      fontSize: 18),
                 ),
               ),
-            ),
+            ]),
+            Padding(
+              padding: EdgeInsets.only(top: 60),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: RaisedButton(
+                  color: Color.fromRGBO(1, 44, 50, 0.8),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Screen13()),
+                    );
+                  },
+                  child: Text(
+                    'CREATE STORE NOW',
+                    style: TextStyle(
+                        color: Colors.white,
+                        // fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
